27a0362b89ec7192d450182a36227ea282af648c
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2016-04-21 23:18:30 -0400

    Add the function lydoc.collector.parse

--- lydoc/collector.py
+++ lydoc/collector.py
@@ -3,6 +3,9 @@ from Lilypond files and directories."""
 
 from lydoc.lilyparser import LilyParser, LilySemantics
 import re
+import os
+from grako.exceptions import FailedParse, FailedSemantics
+import logging
 
 
 # These two regular expressions are used by the strip_leading_comment function
@@ -11,7 +14,7 @@ _whitespace_only_re = re.compile('^[ \t%]+$', re.MULTILINE)
 _leading_whitespace_re = re.compile('(^[ \t%]*)(?:[^ \t\n])', re.MULTILINE)
 
 
-def strip_leading_comment(text):
+def strip_leading_comments(text):
     """Strips the leading whitespaces and % from the given text.
 
     Adapted from textwrap.dedent
@@ -52,7 +55,7 @@ def strip_leading_comment(text):
     return text
 
 
-class DocumentationSemantics(LydocSemantics):
+class DocumentationSemantics(LilySemantics):
     """Semantic actions to be performed during Grako parsing.
 
     See:
@@ -82,7 +85,7 @@ class DocumentationSemantics(LydocSemantics):
         return ast
 
     def name_definition(self, ast):
-        LOGGER.debug('Found name definition %s', ast.name)
+        logging.debug('Found name definition %s', ast.name)
         self.add_position_info(ast)
         # Convert the AST to a simple dict, so that the grako buffer
         # associated with the parseinfo can be released when it is no
@@ -105,6 +108,66 @@ class DocumentationSemantics(LydocSemantics):
         buffer = pinfo.buffer
         file = buffer.filename
         line = buffer.line_info(pinfo.pos).line
-        LOGGER.error('Error while parsing scheme. (%s:%s)', file, line)
+        logging.error('Error while parsing scheme. (%s:%s)', file, line)
         raise FailedSemantics(
             'Error parsing embedded scheme. Unbalanced parentheses?')
+
+
+
+def parse(target, trace=False, **kwargs):
+    """Parse the given target. If it is a file-like object, then parse its
+    contents. If given a string, perform one of the following actions
+
+     - If the string is a valid file path, then open and parse it
+     - If the string is a valid directory path, then recursively look for
+       files ending in .ly or .ily
+     - Otherwise parse the string directly.
+
+    """
+
+    # Beware! This function, that actually is the core of all the
+    # business, is written to minimize the responsibilities of each
+    # chunk of code, keeping things simple. Performance may degrade
+    # because of this, but without actual measurements the simplest
+    # choice is the best one.
+
+    if hasattr(target, 'read'):
+        # It's a file-like object
+        file_content = target.read()
+        return parse(file_content, trace, **kwargs)
+
+    if os.path.isfile(target):
+        if target.endswith(".ily") or target.endswith(".ly"):
+            logging.info("Parsing {}".format(target))
+            with open(target, "r", encoding="utf-8") as fp:
+                return parse(fp, trace, filename=target)
+        else:
+            return []
+
+    if os.path.isdir(target):
+        docs = []
+        logging.info("Parsing directory {}".target)
+        for root, _, files in os.walk(target):
+            for f in files:
+                fname = os.path.join(root, f)
+                file_docs = parse(fname, trace)
+                docs.extend(file_docs)
+        return docs
+
+    # We were given a text, so parse it directly
+    docs = []
+    parser = LilyParser(parseinfo=True)
+    try:
+        parser.parse(target,
+                     'lilypond',
+                     semantics=DocumentationSemantics(docs),
+                     filename=kwargs.get("filename", None),
+                     trace=trace)
+    except FailedParse as err:
+        # Do something on error
+        pass
+    except RuntimeError as err:
+        # Do something on error
+        pass
+
+    return docs
