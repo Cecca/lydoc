dadff6e7f14611365002cd8d75961b8a325276bc
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2016-04-21 23:33:53 -0400

    Start work on main function

---
+++ lydoc/__init__.py
@@ -0,0 +1,50 @@
+"""Lydoc is a simple API documentation generator for lilypond."""
+
+from . import collector
+import argparse
+import sys
+import logging
+import pprint
+
+
+def _cli_argument_parser():
+    argp = argparse.ArgumentParser(
+        description='Produce Lilypond documentation')
+
+    argp.add_argument(
+        'path', metavar='PATH',
+        help='The file or directory to parse')
+
+    argp.add_argument(
+        '--output', '-o', metavar='FILE',
+        help='The output file. If not given, prints to standard output')
+
+    argp.add_argument(
+        '--trace-parser', action='store_true',
+        help='Print debug information from the parser')
+
+    argp.add_argument(
+        '-d', '--debug',
+        help="Detailed debugging information",
+        action="store_const", dest="loglevel", const=logging.DEBUG,
+        default=logging.WARNING)
+    argp.add_argument(
+        '-v', '--verbose',
+        help="Verbose output",
+        action="store_const", dest="loglevel", const=logging.INFO)
+
+    return argp
+
+
+def main():
+    """The main entry point of the program"""
+
+    # Parse command line arguments
+    argp = _cli_argument_parser()
+    args = argp.parse_args()
+
+    # setup logging
+    logging.basicConfig(level=args.loglevel)
+
+    docs = collector.parse(args.path, args.trace)
+    pprint(docs)
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     },
     entry_points = {
         'console_scripts': [
-            # TODO
+            'lydoc=lydoc:main'
         ]
     }
 )
